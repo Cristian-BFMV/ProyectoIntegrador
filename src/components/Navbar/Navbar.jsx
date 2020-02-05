b6fa654ccecd9e903b9ebba5ba75497500c0ee53
Cristian Mendoza <ccamilo.mendoza@udea.edu.co>
2020-02-04 22:58:07 -0500

    Resposive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,16 @@ import { Link, NavLink } from 'react-router-dom';
 import './Navbar.css';
 const Navbar = () => {
   const activeLink = { color: 'rgba(230, 230, 230, 0.5)' };
+  const navSlide = () => {
+    const navList = document.querySelector('.nav-list');
+    const burger = document.querySelector('.burger-menu');
+    navList.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+  };
+
   return (
     <nav className="main-nav">
-      <div className="nav-brand">
+      <div className="nav-brand-container">
         <Link to="/" className="nav-brand">
           <h1>Rappi Makers</h1>
         </Link>
@@ -37,7 +44,7 @@ const Navbar = () => {
           </a>
         </li>
       </ul>
-      <div className="burger-menu">
+      <div className="burger-menu" onClick={() => navSlide()}>
         <div className="line1"></div>
         <div className="line2"></div>
         <div className="line3"></div>
